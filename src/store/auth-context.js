2f5263d4e2ce7b62090ff52f90a3ade380f89d0a
SrinivasaKamathR <kamathsri1997@gmail.com>
2023-01-14 11:39:02 +0530

    managing the UI for users based on whether they are logged-In or not

---
+++ src/store/auth-context.js
@@ -0,0 +1,37 @@
+import { createContext, useState } from "react";
+
+const AuthContext = createContext({
+  token: "",
+  isLoggedIn: false,
+  login: (token) => {},
+  logout: () => {},
+});
+
+export const AuthContextProvider = (props) => {
+  const [token, setToken] = useState(null);
+
+  const userIsLoggedIn = !!token;
+
+  const loginHandler = (token) => {
+    setToken(token);
+  };
+
+  const logoutHanlder = () => {
+    setToken(null);
+  };
+
+  const contextValue = {
+    token: token,
+    isLoggedIn: userIsLoggedIn,
+    login: loginHandler,
+    logout: logoutHanlder,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
